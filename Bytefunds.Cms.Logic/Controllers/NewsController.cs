71c2ba609123e889f1a8ad545a20b28aba58dabc
simple.jiang <jiangchun1320@hotmail.com>
2015-08-25 16:57:30 +0800

    modify

--- Bytefunds.Cms.Logic/Controllers/NewsController.cs
+++ Bytefunds.Cms.Logic/Controllers/NewsController.cs
@@ -17,7 +17,20 @@ namespace Bytefunds.Cms.Logic.Controllers
         public ActionResult Get(int key)
         {
             IContent content = ApplicationContext.Services.ContentService.GetById(key);
-            return Json(new { title = content.GetValue<string>("title"), content = content.GetValue<string>("content"), id = content.Id }, JsonRequestBehavior.AllowGet);
+            switch (content.ContentType.Alias.ToLower())
+            {
+                case "productelement":
+                    return Json(new
+                    {
+                        title = content.GetValue<string>("title"),
+                        content = content.GetValue<string>("content"),
+                        id = content.Id,
+                        canbuy = content.GetValue<bool>("canBuy")
+                    }, JsonRequestBehavior.AllowGet);
+                default:
+                    return Json(new { title = content.GetValue<string>("title"), content = content.GetValue<string>("content"), id = content.Id }, JsonRequestBehavior.AllowGet);
+            }
+
         }
 
     }
