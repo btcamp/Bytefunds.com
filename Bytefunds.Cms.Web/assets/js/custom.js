a6ac181c5c8a7d5f14faa3ddfe0ca3e049ed82cd
simple.jiang <jiangchun1320@hotmail.com>
2015-09-15 10:15:11 +0800

    delete

--- Bytefunds.Cms.Web/assets/js/custom.js
+++ Bytefunds.Cms.Web/assets/js/custom.js
@@ -155,6 +155,8 @@ jQuery(function ($) {
         ByteFunds.ObtainNews2();
         ByteFunds.hiddenModal();
         ByteFunds.showModal();
+        ByteFunds.showCircle();
+        ByteFunds.showCharts();
     });
     ByteFunds.qqService = function (qq) {
         var qq_list = new Array('626770319');
@@ -229,6 +231,41 @@ jQuery(function ($) {
         var modal = $(".modal");
         btn.click(function () {
             modal.attr("class", "modal animated fadeIn");
-        })
+        });
     };
+    ByteFunds.showCircle = function () {
+        $('#indicatorContainer').radialIndicator({
+            barColor: '#F5B024',
+            barWidth: 5,
+            initValue: 25.2,
+            roundCorner: true,
+            percentage: true,
+            radius: 30,
+            fontFamily: 'Calibri'
+        });
+    };
+    ByteFunds.showCharts = function () {
+        $('#charts').highcharts({
+            chart: {
+            },
+            title: {
+                text: '零钱计划第一季 一周收益',
+                style: { color: "#F5B024", fontSize: "1.8em", fontFamily: '黑体', fontWeight: 600 }
+            },
+            xAxis: {
+                categories: ['09-06', '09-07', '09-08', '09-09', '09-10', '09-11', '09-12']
+            },
+            series: [{
+                type: 'spline',
+                name: '当日收益',
+                data: [0.121, 0.125, 0.123, 0.124, 0.131, 0.134, 0.135],
+                marker: {
+                    lineWidth: 3,
+                    lineColor: Highcharts.getOptions().colors[3],
+                    fillColor: 'white'
+                },
+                itemStyle: { color: 'red' }
+            }]
+        });
+    }
 });
